84b5c6f289369ac79d7505447931a8f771c9e6bd
Emily Anderson <eaanders@iastate.edu>
2017-11-12 13:40:56 -0600

    fake login and lock unlock

--- App.js
+++ App.js
@@ -1,17 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
+import { StackNavigator, } from 'react-navigation';
+import ButtonComponent, { CircleButton, RoundButton, RectangleButton } from 'react-native-button-component';
 
-export default class App extends React.Component {
+class LoginScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Login',
+  };
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text>Open down App.css to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <TextInput style={{height: 50}}
+        placeholder='Username'
+        onChangeText={(text) => this.setState({text})}
+        />
+        <TextInput style={{height: 50}}
+        placeholder='Password'
+        onChangeText={(text) => this.setState({text})}
+        />
+        <Button color='#32CD32' title="Login" onPress={() => navigate('Home')}/>
       </View>
     );
   }
 }
+
+class HomeScreen extends React.Component {
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    buttonTitle: 'Unlock', // default button color goes here
+    };
+  }
+  static navigationOptions = {
+    title: 'Home',
+  };
+  _onButtonPress = () => {
+    if (this.state.buttonTitle == 'Unlock') {
+      this.setState({ buttonTitle: 'Lock'});
+    }
+    else {
+      this.setState({ buttonTitle: 'Unlock'});
+    }
+  }
+  render() {
+    const { goBack } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <Text> Welcome </Text>
+        <Text> Press to lock/unlock. </Text>
+        <Button color='#32CD32' title={this.state.buttonTitle} onPress={() => this._onButtonPress()}/>
+        <Button color='#D3D3D3' title='Logout' onPress={() => goBack()}/>
+      </View>
+    );
+  }
+}
+
+const App = StackNavigator ({
+  Login: { screen: LoginScreen },
+  Home: { screen: HomeScreen },
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -21,3 +70,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
