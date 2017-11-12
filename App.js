45d1148335c03193b9218ee12207f2d27507ad42
Emily Anderson <eaanders@iastate.edu>
2017-11-12 14:27:44 -0600

    username and password required

--- App.js
+++ App.js
@@ -4,22 +4,42 @@ import { StackNavigator, } from 'react-navigation';
 import ButtonComponent, { CircleButton, RoundButton, RectangleButton } from 'react-native-button-component';
 
 class LoginScreen extends React.Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    username: false,
+    password: false,
+    };
+  }
   static navigationOptions = {
     title: 'Login',
   };
+  _onLogin = () => {
+    if (this.state.username == false || this.state.password == false) {
+      Alert.alert('Enter both a username and password');
+    }
+    else {
+      this.props.navigation.navigate('Home');
+    }
+  }
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <TextInput style={{height: 50}}
+        <TextInput
+        style={{height: 50}}
         placeholder='Username'
         onChangeText={(text) => this.setState({text})}
+        onSubmitEditing={() => this.setState({ username: true })}
         />
-        <TextInput style={{height: 50}}
+        <TextInput
+        style={{height: 50}}
         placeholder='Password'
         onChangeText={(text) => this.setState({text})}
+        secureTextEntry={true}
+        onSubmitEditing={() => this.setState({ password: true })}
         />
-        <Button color='#32CD32' title="Login" onPress={() => navigate('Home')}/>
+        <Button color='#32CD32' title="Login" onPress={() => this._onLogin()}/>
       </View>
     );
   }
@@ -28,9 +48,9 @@ class LoginScreen extends React.Component {
 class HomeScreen extends React.Component {
   constructor(props) {
   super(props);
-
   this.state = {
-    buttonTitle: 'Unlock', // default button color goes here
+    buttonTitle: 'Unlock',
+    currentState: 'Locked',
     };
   }
   static navigationOptions = {
@@ -39,17 +59,20 @@ class HomeScreen extends React.Component {
   _onButtonPress = () => {
     if (this.state.buttonTitle == 'Unlock') {
       this.setState({ buttonTitle: 'Lock'});
+      this.setState({ currentState: 'Unlocked' });
     }
     else {
       this.setState({ buttonTitle: 'Unlock'});
+      this.setState({ currentState: 'Locked' });
     }
   }
   render() {
     const { goBack } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text> Welcome </Text>
-        <Text> Press to lock/unlock. </Text>
+        <Text> Welcome! </Text>
+        <Text> Your Apartment is {this.state.currentState}. </Text>
+        <Text> Press to {this.state.buttonTitle}. </Text>
         <Button color='#32CD32' title={this.state.buttonTitle} onPress={() => this._onButtonPress()}/>
         <Button color='#D3D3D3' title='Logout' onPress={() => goBack()}/>
       </View>
