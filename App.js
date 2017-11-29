8b024ec5a523b1d9d660695cff0b69eb10c81e43
Emily Anderson <eaanders@iastate.edu>
2017-11-29 14:09:46 -0600

    relocks, displays logs, logs out, functions with fake info

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
-import { StackNavigator, } from 'react-navigation';
+import { StackNavigator, NavigationActions } from 'react-navigation';
 import ButtonComponent, { CircleButton, RoundButton, RectangleButton } from 'react-native-button-component';
 
 class LoginScreen extends React.Component {
@@ -19,6 +19,12 @@ class LoginScreen extends React.Component {
       Alert.alert('Enter both a username and password');
     }
     else {
+      // authenticate
+      // get info from server
+      global.userID = 1; // get from server
+      global.lockID = 1; // get from server
+      global.lockLog = [{uid: 2, time: '1:00 PM'},{uid: 4, time: '11:15 AM'},{uid: 2, time: '9:30 AM'},{uid: 3, time: '7:00 AM'}]; // get from server
+      global.numInside = 2; // get from server
       this.props.navigation.navigate('Home');
     }
   }
@@ -57,38 +63,88 @@ class HomeScreen extends React.Component {
   this.state = {
     buttonTitle: 'Unlock',
     currentState: 'Locked',
+    relockMessage: '',
+    timePassed: false,
     };
   }
   static navigationOptions = {
     title: 'Home',
   };
+  _onLogout = () => {
+    this.props.navigation.dispatch(resetAction);
+  }
   _onButtonPress = () => {
-    if (this.state.buttonTitle == 'Unlock') {
+    if (this.state.currentState == 'Locked') { // unlocking the door
       this.setState({ buttonTitle: 'Lock'});
       this.setState({ currentState: 'Unlocked' });
+      this.setState({ relockMessage: 'The door will automatically relock in 30 seconds.' });
+      setTimeout(() => this._relock(), 5000); // 5 seconds currently
     }
-    else {
+    else { // locking the door
       this.setState({ buttonTitle: 'Unlock'});
       this.setState({ currentState: 'Locked' });
+      this.setState({ relockMessage: '' });
+    }
+  }
+  _relock = () => {
+    if (this.state.currentState == 'Unlocked') {
+      this._onButtonPress();
     }
   }
   render() {
-    const { goBack } = this.props.navigation;
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text> Welcome! </Text>
-        <Text> Your Apartment is {this.state.currentState}. </Text>
+        <Text> Lock #{global.lockID} is {this.state.currentState}.</Text>
+        <Text> There are currently {global.numInside} residents inside. {'\n'} {'\n'} </Text>
         <Text> Press to {this.state.buttonTitle}. </Text>
         <Button color='#32CD32' title={this.state.buttonTitle} onPress={() => this._onButtonPress()}/>
-        <Button color='#D3D3D3' title='Logout' onPress={() => goBack()}/>
+        <Text> {this.state.relockMessage} {'\n'}</Text>
+        <Button color='#32CD32' title='View Logs' onPress={() => navigate('Log')} />
+        <Button color='#D3D3D3' title='Logout' onPress={() => this._onLogout()} />
       </View>
     );
+  }
+}
+
+const resetAction = NavigationActions.reset({
+      index: 0,
+      actions: [NavigationActions.navigate({ routeName: 'Login' })],
+    });
+
+class LogScreen extends React.Component {
+  constructor(props) {
+  super(props);
+  }
+  static navigationOptions = {
+    title: 'Log',
+  };
+  _onLogout = () => {
+    this.props.navigation.dispatch(resetAction);
+  }
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <Text> Logs for lock #{global.lockID}: {'\n'}{'\n'}</Text>
+        {this.listLockLogs()}
+        <Button color='#D3D3D3' title='Logout' onPress={() => this._onLogout()}/>
+      </View>
+    );
+  }
+  listLockLogs() {
+    return global.lockLog.map((data) => {
+      return (
+        <View><Text> User #{data.uid} at {data.time} {'\n'} </Text></View>
+      )
+    })
   }
 }
 
 const App = StackNavigator ({
   Login: { screen: LoginScreen },
   Home: { screen: HomeScreen },
+  Log: { screen: LogScreen },
 });
 
 const styles = StyleSheet.create({
